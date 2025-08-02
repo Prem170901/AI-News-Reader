7c94809531d5d6a8ebb3e29e19da753c84b8f2bc
Prem Kumar K.M <128566945+Prem170901@users.noreply.github.com>
2025-08-02 22:55:50 +0530

    Add files via upload

---
+++ AI_news_reader/client/build/precache-manifest.2e46fa6d7269c0dbc7c6d85846c6761f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddb6ced3bcdaf2e3805428892a62de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de07733f62ca9cbddd36",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "a707ed8cbe683f8b9585",
+    "url": "/static/js/2.306ce8ec.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.306ce8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de07733f62ca9cbddd36",
+    "url": "/static/js/main.cfaec880.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
